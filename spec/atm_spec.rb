5d0e2898e382ba1c7208050c36602379082cad0f
rmzse <rodrigo@therawdelicious.com>
2016-11-14 22:06:32 +0100

    adds accound disable expectation to spec

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe Atm do
-  let(:account) {instance_double('Account', pin_code: '1234', exp_date: '04/17')}
+  let(:account) {instance_double('Account', pin_code: '1234', exp_date: '04/17', account_status: :active)}
 
   before do
     allow(account).to receive(:balance).and_return (100)
@@ -42,6 +42,13 @@ describe Atm do
     allow(account).to receive(:exp_date).and_return('12/15')
     expected_output = { status: false, message: 'card expired', date: Date.today }
     expect(subject.withdraw(5, '1234', account)).to eq expected_output
+  end
+
+  it 'rejects withdrawals if account is :disabled' do
+    #allow(account).to receive(:account_status).and_return(:disabled)
+    let(:account) {instance_double('Account', pin_code: '1234', exp_date: '04/17', account_status: :disabled)}
+    expected_output = { status: false, message: 'account disabled', date: Date.today }
+    expect(subject.withdraw(5, '1234, account')).to eq expected_output
   end
 
 end
