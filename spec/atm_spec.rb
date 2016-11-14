6a61c3258261ef00c7f60e47e4988bcd3f068d92
rmzse <rodrigo@therawdelicious.com>
2016-11-14 21:33:17 +0100

    adds pin code instance double arg to specs and updates all withdraw method calls in spec

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe Atm do
-  let(:account) {instance_double('Account')}
+  let(:account) {instance_double('Account', pin_code: '1234')}
 
   before do
     allow(account).to receive(:balance).and_return (100)
@@ -13,24 +13,24 @@ describe Atm do
   end
 
   it 'reduces funds with withdrawals' do
-    subject.withdraw(50, account)
+    subject.withdraw(50, '1234', account)
     expect(subject.funds).to eq 950
   end
 
   it 'allows withdrawals if account has enough balance' do
     expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
-    expect(subject.withdraw(45, account)).to eq expected_output
+    expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
 
   it 'rejects withdrawals if account has insufficient funds' do
     expected_output = { status: false, message: 'insufficient funds in account', date: Date.today }
-    expect(subject.withdraw(105, account)).to eq expected_output
+    expect(subject.withdraw(105, '1234', account)).to eq expected_output
   end
 
   it 'rejects withdrawals if ATM machine has insufficient funds' do
     subject.funds = 50
     expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
-    expect(subject.withdraw(100, account)).to eq expected_output
+    expect(subject.withdraw(100, '1234', account)).to eq expected_output
   end
 
 end
