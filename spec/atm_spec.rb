f452e9c44d66e8e5cc437a6b0d806849df979383
rmzse <rodrigo@therawdelicious.com>
2016-11-14 22:20:29 +0100

    removed single quote from after the account arg in spec expectation for disabled account

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -50,7 +50,7 @@ describe Atm do
     it 'rejects withdrawals if account is disabled' do
       #allow(account).to receive(:account_status).and_return(:disabled)
       expected_output = { status: false, message: 'account disabled', date: Date.today }
-      expect(subject.withdraw(5, '1234', account')).to eq expected_output
+      expect(subject.withdraw(5, '1234', account)).to eq expected_output
     end
   end
 
