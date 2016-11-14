1f22d7853e37a9fff7c8745f005fb384f1813288
rmzse <rodrigo@therawdelicious.com>
2016-11-14 21:50:53 +0100

    adds expectation for expired card

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,7 +1,7 @@
 require './lib/atm.rb'
 
 describe Atm do
-  let(:account) {instance_double('Account', pin_code: '1234')}
+  let(:account) {instance_double('Account', pin_code: '1234', exp_date: '04/17')}
 
   before do
     allow(account).to receive(:balance).and_return (100)
@@ -36,6 +36,12 @@ describe Atm do
   it 'rejects withdrawals if pin code is incorrect' do
     expected_output = { status: false, message: 'wrong pin code', date: Date.today }
     expect(subject.withdraw(50, 9999, account)).to eq expected_output
+  end
+
+  it 'rejects withdrawals if atm card is expired' do
+    allow(account).to receive(:exp_date).and_return('12/15')
+    expected_output = { status: false, message: 'card expired', date: Date.today }
+    expect(subject.withdraw(5, '1234', account)).to eq expected_output
   end
 
 end
