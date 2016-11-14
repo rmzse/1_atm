163c6a6759d8e7da37688e26042513915c44a559
rmzse <rodrigo@therawdelicious.com>
2016-11-14 20:45:37 +0100

    adds account double and balance setter for account double

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,13 @@
 require './lib/atm.rb'
 
 describe Atm do
+  let(:account) {instance_double('Account')}
+
+  before do
+    allow(account).to receive(:balance).and return (100)
+    allow(account).to receive(:balance=)
+  end
+
   it 'has $1000 on initialize' do
     expect(subject.funds).to eq 1000
   end
