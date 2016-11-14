b020f49a7d94f228816d33acfcff26fa563d2e81
rmzse <rodrigo@therawdelicious.com>
2016-11-14 20:37:53 +0100

    adds expectation for funds withdrawals to spec

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -4,4 +4,9 @@ describe Atm do
   it 'has $1000 on initialize' do
     expect(subject.funds).to eq 1000
   end
+
+  it 'reduces funds with withdrawals' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
 end
