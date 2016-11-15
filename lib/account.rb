010bf0b429b4362c1ab4c52db39dcd089c5e12f2
rmzse <rodrigo@therawdelicious.com>
2016-11-15 19:06:53 +0100

    add pin code length as instance var in Acct class

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,12 @@
 class Account
-  attr_accessor :pin_code, :exp_date, :account_status, :account_owner
+  attr_accessor :pin_code, :exp_date, :account_status, :account_owner, :pin_code_length
 
   def initialize
     @pin_code = '1234'
     @exp_date = '04/17'
     @account_status = :active
     @account_owner = 'Rod'
-    pin_code_length = correct_pin_length?(pin_code)
+    @pin_code_length = correct_pin_length?(pin_code)
   end
 
   def correct_pin_length?(number)
