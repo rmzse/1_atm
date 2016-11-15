f96a5094842496fe5dabe382813023eef3bff100
rmzse <rodrigo@therawdelicious.com>
2016-11-15 16:57:08 +0100

    initialize acct owner

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,11 @@
 class Account
-  attr_accessor :pin_code, :exp_date, :account_status
+  attr_accessor :pin_code, :exp_date, :account_status, :account_owner
 
   def initialize
     @pin_code = '1234'
     @exp_date = '04/17'
     @account_status = :active
+    @account_owner = 'Rod'
   end
 
 end
