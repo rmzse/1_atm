c01b9d31901e78baf815e88d6f58f03a6f43d67d
rmzse <rodrigo@therawdelicious.com>
2016-11-15 20:06:02 +0100

    add valid test for randomized pin code on acct creation

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,6 +4,7 @@ require './lib/account.rb'
 describe Account do
 
   it 'has a pincode "1234" on initialize' do
+    #expect(subject.pin_code).to eq subject.pin_code
     expect(subject.pin_code).to eq account.pin_code
     #expect(subject.pin_code).to eq '1234'
   end
