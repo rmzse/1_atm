08c8a862b25edec58bf5930e5b0717cc603f4370
rmzse <rodrigo@therawdelicious.com>
2016-11-16 22:17:15 +0100

    add test for the specific initiated name

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,7 +5,8 @@ describe Person do
   subject { described_class.new(name: Xerxes) }
 
   it 'has a :name on initialize' do
-    expect(subject.name).not_to be nil
+    # expect(subject.name).not_to be nil
+    expect(subject.name).to be 'Xerxes'
   end
 
 end
