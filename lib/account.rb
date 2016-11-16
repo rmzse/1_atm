1728caada1092946d1a3313371a6c99321c91ab7
rmzse <rodrigo@therawdelicious.com>
2016-11-16 16:12:06 +0100

    initialize person in both spec and implementation

--- lib/account.rb
+++ lib/account.rb
@@ -4,11 +4,11 @@ class Account
   STANDARD_VALIDITY_YRS = 5
   attr_accessor :pin_code, :exp_date, :account_status, :account_owner
 
-  def initialize
+  def initialize(arg)
     @pin_code = rand(1000..9999).to_s
     @exp_date = set_card_exp_date
     @account_status = :active
-    @account_owner = 'Rod'
+    @account_owner = arg[:account_owner]
   end
 
   def disable_account
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,14 +3,12 @@ require './lib/account.rb'
 
 describe Account do
   let(:person) {instance_double('Person', name: 'Rod')}
-  #subject { described_class.new( {owner:person} ) }
-
+  subject { described_class.new( {account_owner: person} ) }
 
   it 'has a randomized pincode of 4 digits on initialize' do
-    #pin_code_length = Math.log10(subject.pin_code.to_i).to_i + 1
-    expect(subject.pin_code).to eq Math.log10(subject.pin_code.to_i).to_i + 1
+    pin_code_length = Math.log10(subject.pin_code.to_i).to_i + 1
+    expect(pin_code_length).to eq 4
   end
-
 
   it 'has an ATM card with expiry date of 11/22 on initialize' do
     expect(subject.exp_date).to eq '11/21'
@@ -21,12 +19,16 @@ describe Account do
   end
 
   it 'has an account owner on initialize' do
-    expect(subject.account_owner).to eq 'Rod'
+    expect(subject.account_owner).to eq person
   end
 
   it 'disables an account for a person (instance)' do
     subject.disable_account
     expect(subject.account_status).to eq :disabled
+  end
+
+  it 'raises an error if no owner is set' do
+    expect{ described_class.new }.to raise_error 'Error: An account owner must be set!'
   end
 
 
