bd497e96fea8d5b62fe9c75af8c358f30f4e0903
rmzse <rodrigo@therawdelicious.com>
2016-11-16 21:04:02 +0100

    add person.rb and person_spec.rb

---
+++ spec/person_spec.rb
@@ -0,0 +1,12 @@
+require './lib/person.rb'
+require './lib/atm.rb'
+
+describe Person do
+  # subject { described_class.new(name: Thomas) }
+
+
+  # it 'has a :name on initialize' do
+  #   expect(subject.name). not_to be nil
+  # end
+
+end
