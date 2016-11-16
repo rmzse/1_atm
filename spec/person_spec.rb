d877d950b49dbcc4dd19d96ef491acf6cae02374
rmzse <rodrigo@therawdelicious.com>
2016-11-16 22:22:16 +0100

    simplify spec file by separating the two name tests to own blocks

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,11 +2,15 @@ require './lib/person.rb'
 # require './lib/atm.rb'
 
 describe Person do
-  subject { described_class.new(name: 'Xerxes') }
+  # subject { described_class.new(name: 'Xerxes') }
 
-  it 'has a :name on initialize' do
-    # expect(subject.name).not_to be nil
+  it 'has the name "Xerxes" on initialize' do
     expect(subject.name).to be 'Xerxes'
   end
+
+  # it 'has a :name on initialize' do
+  #   # expect(subject.name).not_to be nil
+  #   expect(subject.name).to be 'Xerxes'
+  # end
 
 end
