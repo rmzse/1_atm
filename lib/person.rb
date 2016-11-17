411a36fcf61786c86254eb8f9471ded64766947c
rmzse <rodrigo@therawdelicious.com>
2016-11-17 13:02:21 +0100

    add conditional check for account not nil in implementation

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
   def initialize(args = {})
     args[:name] != nil ? (@name = args[:name] ) : (raise 'Error: You need a person name!')
     @cash = 0
-    args[:account] == nil ? : (raise 'Error: Account not nil!')
+    raise 'Error: Account not nil!' if args[:account] != nil
   end
 
 end
