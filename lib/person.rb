765885bac2b7f08a6797f865800174b76cfca429
rmzse <rodrigo@therawdelicious.com>
2016-11-17 12:38:33 +0100

    add cash attrib to implementation w value 0

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
-  attr_accessor :name
+  attr_accessor :name, :cash
 
   def initialize(args = {})
     args[:name] != nil ? (@name = args[:name] ) : (raise 'Error: You need a person name!')
-
+    @cash = 0
   end
 
 end
