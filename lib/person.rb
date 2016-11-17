c9371e467abc0835763b7b02fd9350f415ecee20
rmzse <rodrigo@therawdelicious.com>
2016-11-17 16:41:42 +0100

    add arg to #create_account in spec and pass person to it

--- lib/person.rb
+++ lib/person.rb
@@ -5,12 +5,18 @@ class Person
     args[:name] != nil ? (@name = args[:name] ) : (raise 'Error: You need a person name!')
     @cash = 0
     raise 'Error: Account not nil!' if args[:account] != nil
-    # @account = args[:account] if account.respond_to?(:disable_account)
+    # binding.pry
+    @account = args[:account] if account.respond_to?(:disable_account)
+    # binding.pry
     create_account(args[:account])
+    # binding.pry
   end
 
   def create_account(hash_arg)
-    account.respond_to?(:disable_account) ? (@account = args[:account]) : (raise 'Error: Account type mismatch.')
+    # binding.pry
+    @account = args[:account] if account.respond_to?(:disable_account)
+    # code below doesn't pass, since we haven't specified raising an err in the spec
+    # account.respond_to?(:disable_account) ? (@account = args[:account]) : (raise 'Error: Account type mismatch.')
   end
 
 end
